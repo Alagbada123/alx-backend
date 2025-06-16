68e1aed8e47cc3042a16cce0c590abfb4617c697
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-06-16 12:41:16 +0200

    This is a good file

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -10,7 +10,7 @@ client.on('error', (err) => {
 const publishMessage = (message, time) => {
   setTimeout(() => {
     console.log(`About to send ${message}`);
-    client.publish('holberton school channel', message);
+    client.publish('ALX channel', message);
   }, time);
 };
 
@@ -18,7 +18,7 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-publishMessage('Holberton Student #1 starts course', 100);
-publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('ALX Student #1 starts course', 100);
+publishMessage('ALX Student #2 starts course', 200);
 publishMessage('KILL_SERVER', 300);
-publishMessage('Holberton Student #3 starts course', 400);
+publishMessage('ALX Student #3 starts course', 400);
